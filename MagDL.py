489594c4b042d6ee1b77a96da9686be9d5ad8234
Navan Chauhan <navanchauhan@gmail.com>
2019-04-23 23:17:14 +0530

    DRYed Code (Converted Repeating code into functions)

--- MagDL.py
+++ MagDL.py
@@ -2,15 +2,12 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
-
-#userInput = "Computer Active"
 userInput = input("Enter your Input: \n")
 
 searchTerm = userInput.replace(" ", "+")
 firstWord = userInput.split(' ', 1)[0].lower() 
 
 url = "https://magazinelib.com/?s=" + searchTerm
-
 
 response = requests.get(url)
 page = str(BeautifulSoup(response.content, "lxml"))
@@ -19,57 +16,54 @@ allLinks = []
 links = []
 
 def getURL(page):
-    start_link = page.find("a href")
-    if start_link == -1:
-        return None, 0
-    start_quote = page.find('"', start_link)
-    end_quote = page.find('"', start_quote + 1)
-    url = page[start_quote + 1: end_quote]
-    return url, end_quote
 
-while True:
-    url, n = getURL(page)
-    page = page[n:]
-    if url:
-        allLinks.append(url)
-    else:
-        break
+	start_link = page.find("a href")
+	if start_link == -1:
+		return None, 0
+	start_quote = page.find('"', start_link)
+	end_quote = page.find('"', start_quote + 1)
+	url = page[start_quote + 1: end_quote]
+	return url, end_quote
 
+def getLinks(url, myList):
+	response = requests.get(url)
+	page = str(BeautifulSoup(response.content, "lxml"))
+	while True:
+		url, n = getURL(page)
+		page = page[n:]
+		if url:
+			myList.append(url)
+		else:
+			break
 
-for a in allLinks:
-    x = re.search("all/" + firstWord, a)
-    if(x != None):
-        links.append(a)
+def patternMatch(pattern, myList, newList=None):
+	for a in myList:
+		x = re.search(pattern, a)
+		if(x != None):
+			#print(a)
+			if(newList!=None):
+				newList.append(a)
+			else:
+				print(a)
 
 def printLinks(n):
-    i = 0
-    while (i<n) :
-        print("[", i, "]", (links[i].replace("https://magazinelib.com/all/","")).replace("/","").replace("-"," "))
-        i +=1
+	i = 0
+	while (i<n) :
+		print("[", i, "]", (links[i].replace("https://magazinelib.com/all/","")).replace("/","").replace("-"," "))
+		i +=1
+
+getLinks(url, allLinks)
+patternMatch("all/" + firstWord, allLinks, links)
 
 printLinks(10)
 
 choice = eval(input("Enter the index number: \n"))
 
 dl = []
-
 url = links[choice]
-print(links[choice])
-response = requests.get(url)
-page = str(BeautifulSoup(response.content, "lxml"))
 
-while True:
-    url, n = getURL(page)
-    page = page[n:]
-    if url:
-        dl.append(url)
-    else:
-        break
-
-for a in dl:
-    x = re.search("vk.com", a)
-    if(x != None):
-        print(a)
+getLinks(url, dl)
+patternMatch("vk.com", dl)
 
 
 
